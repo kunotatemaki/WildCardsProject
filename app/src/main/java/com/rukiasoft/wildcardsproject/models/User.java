579652b22b25d8ff09aaae970af5325f192f6f41
Kuno Tatemaki <raulfeliz@gmail.com>
2017-06-29 20:31:35 +0200

    Project's boilerplate

---
+++ app/src/main/java/com/rukiasoft/wildcardsproject/models/User.java
@@ -0,0 +1,88 @@
+package com.rukiasoft.wildcardsproject.models;
+
+/**
+ * Created by roll on 26/06/17.
+ */
+public class User {
+
+    // region Member Variables
+    private String pictureUrl;
+    private String userName;
+    private String userAge;
+    private String city;
+    private String profession;
+    private String smokingAttitude;
+    private String wishOfChildren;
+    // endregion
+
+    // region Constructors
+    public User(){
+
+    }
+    // endregion
+
+    // region Getters
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public String getSmokingAttitude() {
+        return smokingAttitude;
+    }
+
+    public String getWishOfChildren() {
+        return wishOfChildren;
+    }
+
+    // endregion
+
+    // region Setters
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public void setSmokingAttitude(String smokingAttitude) {
+        this.smokingAttitude = smokingAttitude;
+    }
+
+    public void setWishOfChildren(String wishOfChildren) {
+        this.wishOfChildren = wishOfChildren;
+    }
+
+    // endregion
+
+}
